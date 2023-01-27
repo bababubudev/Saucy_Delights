a0a13b19cf049f59258d33a98758cbd12ecb2d7c
Zhiyuan Liu <thenukezealot@gmail.com>
2023-01-27 22:42:32 +0200

    auth slice initial set up

---
+++ frontend/src/api/auth/authSlice.ts
@@ -0,0 +1,37 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+// Get user from local storage
+// const user = JSON.parse(localStorage.getItem("user"))
+
+interface AuthState {
+  isError: boolean;
+  isSuccess: boolean;
+  isLoading: boolean;
+  message: string;
+  email: string | null;
+}
+
+const initialState: AuthState = {
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+  email: null,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isLoading = false;
+      state.isError = false;
+      state.isSuccess = false;
+      state.message = "";
+    },
+  },
+  extraReducers: () => {},
+});
+
+export const { reset } = authSlice.actions;
+export default authSlice.reducer;
