3656b0a9b464e7d72de1e6b34e1541eb25ce52d0
Zhiyuan Liu <thenukezealot@gmail.com>
2023-01-27 20:44:18 +0200

    login register page initial HTML

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+
+import { useState, useEffect } from "react";
 
 function Register() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { email, password, password2 } = formData;
+
+  function onChangeHandler(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  function onSubmitHandler(e) {
+    e.preventDefault();
+  }
   return (
-    <div>REGISTER PAGE</div>
-  )
+    <>
+      <section className="form">
+        <form onSubmit={onSubmitHandler}>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            value={email}
+            placeholder="Email"
+            onChange={onChangeHandler}
+          />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            placeholder="Password"
+            onChange={onChangeHandler}
+          />
+          <input
+            type="password"
+            id="password2"
+            name="password2"
+            value={password2}
+            placeholder="Confirm Your Password"
+            onChange={onChangeHandler}
+          />
+          <button type="submit">Register</button>
+        </form>
+      </section>
+    </>
+  );
 }
 
-export default Register
+export default Register;
