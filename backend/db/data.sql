9c264af1f2880cd3038d771e62dcf24c322e04a5
Prabesh Sharma <prwshshrm@gmail.com>
2023-01-30 17:21:29 +0200

    Blueprint for recipe data

--- backend/db/data.sql
+++ backend/db/data.sql
@@ -7,3 +7,17 @@ CREATE TABLE users(
     fav_recipes INTEGER[],
     comments INTEGER[]
 );
+
+CREATE TABLE recipes(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(32) NOT NULL,
+    description VARCHAR(500),
+    nationality VARCHAR(32),
+    main_ingr VARCHAR(32),
+    ingr VARCHAR(32) ARRAY,
+    food_time CHAR,
+    difficulty INTEGER,
+    time_taken INTEGER,
+    rating INTEGER,
+    created_at DATE
+);
