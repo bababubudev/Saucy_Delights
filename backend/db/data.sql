3c183a2509c26f9381f015f587a502e0e3948350
RomaZinkevich <zra1903@mail.ru>
2023-01-31 11:24:04 +0200

    login and register init

--- backend/db/data.sql
+++ backend/db/data.sql
@@ -2,7 +2,7 @@ CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     email VARCHAR(32) NOT NULL UNIQUE,
     name VARCHAR(32) NOT NULL,
-    password VARCHAR(32) NOT NULL,
+    password VARCHAR(100) NOT NULL,
     created_recipes INTEGER[],
     fav_recipes INTEGER[],
     comments INTEGER[]
--- backend/db/queryhandler.js
+++ backend/db/queryhandler.js
@@ -2,17 +2,14 @@ import { pool } from "./dbconfig.js"
 
 pool.connect()
 
-export function queryHandler(queryText,req,res,message)
-{
-    pool.query(queryText)
-    .then((result)=>{
-        message=message ? message : result.rows
-        res.json({message:message})
-    })
-    .catch(err=>{
-        res.status(400)
-        res.json({message:err.message})
-    })
-        
-
+export async function queryHandler(queryText,res)
+{   
+    try{
+    const result = await pool.query(queryText)
+    return {flag:true,message:result};
+    }
+    catch(err){
+        return {flag:false,message:err};
+    }
+    
 }
