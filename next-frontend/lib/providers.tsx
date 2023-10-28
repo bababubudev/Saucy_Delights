10dd769e6237d00d21b370ec0b2811fa36682d11
Zhiyuan Liu <thenukezealot@gmail.com>
2023-10-28 11:17:57 +0300

    nextJS set up

---
+++ next-frontend/lib/providers.tsx
@@ -0,0 +1,9 @@
+/* Core */
+import { Provider } from 'react-redux'
+
+/* Instruments */
+import { reduxStore } from '@/lib/redux'
+
+export const Providers = (props: React.PropsWithChildren) => {
+  return <Provider store={reduxStore}>{props.children}</Provider>
+}
